301f6b11d868fc16efb9069fcae4823d082db3c5
Kristian <Kristian@DESKTOP-3EN3CKT>
2022-01-17 20:27:42 +0100

    Add project files.

---
+++ Krisprojnew/Program.cs
@@ -0,0 +1,8 @@
+﻿Person p = new("Kristian", "Pedersen", new DateTime(1990, 7, 15));
+Console.WriteLine(p.GetFullName());
+Console.WriteLine(p.BirthDateInDanishFormat);
+Console.WriteLine(p.Age);
+Console.WriteLine(p.TimeSpanInTotalDays);
+
+Person p2 = new();
+Console.WriteLine(p2.GetFullName());
