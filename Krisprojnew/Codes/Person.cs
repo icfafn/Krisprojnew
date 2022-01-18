49f5fe6fcc6625a0004081220c7e1418356eef99
Kristian Pedersen <Icfafn@gmail.com>
2022-01-18 08:33:07 +0100

    Person

--- Krisprojnew/Codes/Person.cs
+++ Krisprojnew/Codes/Person.cs
@@ -27,7 +27,25 @@ namespace Krisprojnew.Codes
                 BirthDateInDanishFormat = birthDate.ToString("dd-MM-yyyy");
                 Age = DateTime.Now.Year - birthDate.Year;
                 TimeSpan ts = DateTime.Now - birthDate;
-                TimeSpanInTotalDays = ts.TotalDays;
+
+            //int ss;
+            //bool pass = int.TryParse("16A", out ss);
+            //if (pass)
+            //{
+            //    Console.WriteLine("Does not match!");
+            //}
+
+            //int tstd = Convert.ToInt16(TimeSpanInTotalDays);
+            //Console.WriteLine("Converted to int " + tstd);
+
+            //object dblboxed = TimeSpanInTotalDays;
+            ////Her bliver den pakket ud. 
+            ////Hvis man ikke ved hvilken type den er pakket ind som, så kan man bruge TryCatch
+            //// til at finde ud af hvilken type det er. 
+            //double unboxed = (double)dblboxed;
+
+
+
             }
 
             public Person()
@@ -42,5 +60,6 @@ namespace Krisprojnew.Codes
             }
         }
     }
+//Object type er værdi i en kasse. 
 
 
